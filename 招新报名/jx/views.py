364111853b78984bf63c65ce26560739de6e9c9b
hqt <heqiut@qq.com>
2017-07-25 05:21:53 +0800

    s

--- 招新报名/jx/views.py
+++ 招新报名/jx/views.py
@@ -6,18 +6,21 @@ from django import forms
 from datetime import datetime,timedelta
 
 
+global list=['127.0.0.1']
+
 def add(req):
     if req.method == 'POST':
         if req.META=="":
 	    return HttpResponse("不要搞事情")
-	ip=req.META['REMOTE_ADDR'] 
+	ip=req.META['REMOTE_ADDR']
+	list.append(ip) 
 	name=req.POST['name']+'|'
 	sex=req.POST['sex']+'|'
 	tel=req.POST['tel']+'|'
 	qq=req.POST['qq']+'|'
 	bumen1=req.POST['bumen1']+'|'
 	select2=req.POST['select2']+'|'
-	induce=req.POST['induce']+'|'
+	induce=req.POST['induce']+'|<br>\n'
 	if name=="|":
 	    return HttpResponse("不要搞事情")
 	if sex=='人妖|':
@@ -25,10 +28,9 @@ def add(req):
 	if tel=="|":
 	    return HttpResponse("不要搞事情")
 
-	strs=open("doc/res.txt").read()
 	if induce.encode("utf-8") in strs:
 	   return HttpResponse("提交失败请联系管理员 ErrorCode:101")
-	if ip in strs:
+	if ip in list:
 	   return HttpResponse("提交失败请联系管理员 ErrorCode:102")
 	if name.encode("utf-8") in strs:
 	   f=open("doc/sb.txt",'a')
@@ -37,7 +39,7 @@ def add(req):
 	   f.close()
 	   return HttpResponse("你已经报过了")
 	f=open("doc/res.txt",'a')
-	res=name+sex+tel+qq+bumen1+select2+induce+ip+'<br>\n'
+	res=name+sex+tel+qq+bumen1+select2+induce
 	f.write(res.encode("utf-8"))
 	f.close()
 	return HttpResponse("报名成功喽")
